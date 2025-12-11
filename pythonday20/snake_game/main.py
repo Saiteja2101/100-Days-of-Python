92b402e8f8dde6cf24bdb9b1728bafed1891608a
Saiteja2101 <bsaiteja2101@gmail.com>
2025-12-11 19:55:47 +0100

    day 20 of learning python (snake game)

--- pythonday20/snake_game/main.py
+++ pythonday20/snake_game/main.py
@@ -1,4 +1,6 @@
-from turtle import Turtle, Screen       #  Importing classes Turtle and Screen from turtle file
+from turtle import Screen       #  Importing classes Turtle and Screen from turtle file
+from paamu import Paamu
+
 import time
 
 screen = Screen()                       # Creatinmg an Object name screen from Screen Class
@@ -7,55 +9,28 @@ screen.bgcolor("black")                 # Setting the screen background colour
 screen.title("Snake Game")              # Creating title for the screen
 screen.tracer(0)
 
-# def move_forward():
-#     snake[0].forward(20)
-# def move_backward():
-#     snake[0].backward(20)
-def move_anticlockwise():
-    snake[0].left(90)
-def move_clockwise():
-    snake[0].right(90)
-# def clear_screen():
-#     snake.clear()
-#     snake.penup()
-#     snake.home()
-#     snake.pendown()
+python = Paamu()
+
+head = python.snake[0]
 
 
-cordinates = [(0,0),(-20,0),(-40,0)]
-
-snake= []
-
-for i in range(3):
-    turtles = Turtle(shape="square")
-    turtles.color("white")
-    turtles.penup()
-    turtles.goto(cordinates[i])
-    snake.append(turtles)
-
-
+screen.listen()
+screen.onkey(python.move_up, "w")
+screen.onkey(python.move_down, "s")
+screen.onkey(python.move_left, "a")
+screen.onkey(python.move_right, "d")
 
 snake_move = True
-
 
 while snake_move:
     screen.update()
     time.sleep(0.15)
-    for snakes in range(len(snake)-1, 0, -1):
-        new_x = snake[snakes-1].xcor()
-        new_y = snake[snakes-1].ycor()
-        snake[snakes].goto(new_x,new_y)
-    snake[0].forward(20)
-    # snake[0].left(90)
-    screen.listen()
-    # screen.onkey(key="w", fun=move_forward)
-    # screen.onkey(key="s", fun=move_backward)
-    screen.onkey(key="a", fun=move_anticlockwise)
-    screen.onkey(key="d", fun=move_clockwise)
-    # screen.onkey(key="space",fun=clear_screen)
-    # if snake.xcor() > 270:
-    #     snake_move = False
-        
+    python.move()
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
+        snake_move = False
+
+            
+
 
 
 screen.exitonclick()                    # Close the screen whenever click on the screen 
