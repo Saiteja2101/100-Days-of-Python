c3dae5426c69ea2aaad5ad5d75f5ed68b19c4b52
Saiteja2101 <bsaiteja2101@gmail.com>
2025-12-11 18:18:30 +0100

    pyhtonday 20 learning (snake game)

--- pythonday20/snake_game/main.py
+++ pythonday20/snake_game/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen       #  Importing classes Turtle and Screen from turtle file
+import time
 
 screen = Screen()                       # Creatinmg an Object name screen from Screen Class
 screen.setup(width=600,height=600)      # Setting the screen size by given width and height
@@ -6,19 +7,19 @@ screen.bgcolor("black")                 # Setting the screen background colour
 screen.title("Snake Game")              # Creating title for the screen
 screen.tracer(0)
 
-def move_forward():
-    snake.forward(20)
-def move_backward():
-    snake.backward(20)
-def move_anticlockwise():
-    snake.left(90)
-def move_clockwise():
-    snake.right(90)
-def clear_screen():
-    snake.clear()
-    snake.penup()
-    snake.home()
-    snake.pendown()
+# def move_forward():
+#     snake.forward(20)
+# def move_backward():
+#     snake.backward(20)
+# def move_anticlockwise():
+#     snake.left(90)
+# def move_clockwise():
+#     snake.right(90)
+# def clear_screen():
+#     snake.clear()
+#     snake.penup()
+#     snake.home()
+#     snake.pendown()
 
 
 cordinates = [(0,0),(-20,0),(-40,0)]
@@ -39,14 +40,15 @@ snake_move = True
 
 while snake_move:
     screen.update()
+    time.sleep(0.25)
     for snakes in snake:
         snakes.forward(20)
-        screen.listen()
-        screen.onkey(key="w", fun=move_forward)
-        screen.onkey(key="s", fun=move_backward)
-        screen.onkey(key="a", fun=move_anticlockwise)
-        screen.onkey(key="d", fun=move_clockwise)
-        screen.onkey(key="space",fun=clear_screen)
+        # screen.listen()
+        # screen.onkey(key="w", fun=move_forward)
+        # screen.onkey(key="s", fun=move_backward)
+        # screen.onkey(key="a", fun=move_anticlockwise)
+        # screen.onkey(key="d", fun=move_clockwise)
+        # screen.onkey(key="space",fun=clear_screen)
         if snakes.xcor() > 270:
             snake_move = False
         
