cb53d9babe08a5b401f0110a905ac9a85f9b4897
Saiteja2101 <bsaiteja2101@gmail.com>
2025-12-13 18:13:07 +0100

    pythonday23 learning (pong game)

--- pythonday22/main.py
+++ pythonday22/main.py
@@ -1,6 +1,8 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -11,8 +13,7 @@ screen.tracer(0)
 bat1 = Paddle((-350, 0))
 bat2 = Paddle((350, 0))
 ball = Ball()
-
-
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(bat1.move_up, "w")
@@ -23,5 +24,28 @@ screen.onkey(bat2.move_down, "Down")
 game_on = True
 while game_on:
     screen.update()
+    time.sleep(0.08)
+    ball.move()
 
+    # Wall bounce (top & bottom)
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Paddle bounce (right paddle)
+    if ball.distance(bat2) < 50 and ball.xcor() > 320:
+        ball.bounce_x()
+
+    # Paddle bounce (left paddle)
+    if ball.distance(bat1) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Ball out of bounds
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_increase()
+    
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_increase()
+    
 screen.exitonclick()
